e79b32bc4b4aa730feceab2536fdf23a561b97b4
StudyingCui <1005586890@qq.com>
2023-02-16 15:24:45 +0800

    fix openssl bio base64

--- src/cplatform/ctools.cpp
+++ src/cplatform/ctools.cpp
@@ -1,4 +1,10 @@
 #include "ctools.h"
+#include <openssl/md5.h>
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/sha.h>
 #ifdef _WIN32
 #include <io.h>
 #else
@@ -53,4 +59,83 @@ CCOME_API std::string getDirData(std::string path) {
     data = data.substr(0, data.size() - 1);
   }
   return data;
+}
+
+
+CCOME_API int base64Encode(const unsigned char *in, int len, char *outBase64) {
+  if (!in || len <= 0 || !outBase64) {
+    return 0;
+  }
+  // 编码内存源，用于存储结果
+  auto memBio = BIO_new(BIO_s_mem());
+  if (!memBio) {
+    return 0;
+  }
+
+  // base64 过滤器
+  auto b64Bio = BIO_new(BIO_f_base64());
+  if (!b64Bio) {
+    BIO_free(memBio);
+    return 0;
+  }
+
+  // 设置 64 字节不加换行符
+  BIO_set_flags(b64Bio, BIO_FLAGS_BASE64_NO_NL);
+
+  // 形成 BIO 链表 : b64Bio--memBio
+  BIO_push(b64Bio, memBio);
+
+  // 往链表头部写入，base64过滤器处理后转入下一个节点(链尾)
+  // 对链表写入会调用编码
+  int res = BIO_write(b64Bio, in, len);
+  if (res <= 0) {
+    // 清理链表
+    BIO_free_all(b64Bio);
+    return 0;
+  }
+
+  // 刷新缓存，写入链表的 mem
+  BIO_flush(b64Bio);
+
+  int outSize = 0;
+  BUF_MEM *pData = nullptr;
+  // 从链表输出源内存 memBio 中读取
+  BIO_get_mem_ptr(b64Bio, &pData);
+  if (pData) {
+    memcpy(outBase64, pData->data, pData->length);
+    outSize = pData->length;
+  }
+  BIO_free_all(b64Bio);
+  return outSize;
+}
+
+CCOME_API int base64Decode(const char *in, int len, unsigned char *outData) {
+  if (!in || len <= 0 || !outData) {
+    return 0;
+  }
+  // 解码内存源，用于密文数据输入
+  auto memBio = BIO_new_mem_buf(in, len);
+  if (!memBio) {
+    return 0;
+  }
+
+  // base64 过滤器
+  auto b64Bio = BIO_new(BIO_f_base64());
+  if (!b64Bio) {
+    BIO_free(memBio);
+    return 0;
+  }
+
+  // 设置 64 字节不加换行符
+  BIO_set_flags(b64Bio, BIO_FLAGS_BASE64_NO_NL);
+
+  // 形成 BIO 链表 : b64Bio--memBio
+  BIO_push(b64Bio, memBio);
+
+  // 读取解码后的数据
+  size_t size = 0;
+  BIO_read_ex(b64Bio, outData, len, &size);
+
+  BIO_free_all(b64Bio);
+  return size;
 }
